ae9ff4d2db3c7aee972685b9980d95feb89846ba
robak <robakkub@gmail.com>
2023-07-20 21:15:41 +0200

    Zadania domowe/2 pierwsze

--- ZadaniaDomowe/test_task3.py
+++ ZadaniaDomowe/test_task3.py
@@ -5,12 +5,17 @@
 # Skrypt powinien obsługiwać różne metody HTTP oraz różne typy danych (takie jak JSON, XML).
 
 import requests
+import json
+import pprint
 
-url = "https://swapi.dev/api"
-params = {"population": "1000"}
+url = "https://swapi.dev/api/planets"
+planetName = input("Wybierz planetę: Tatooine, Alderaan, Yavin IV, Hoth, Dagobah, Bespin, Endor, Naboo, Coruscant, Kamino.>")
+params = {"name":planetName}
 
 response = requests.get(url, params)
+
 if response.status_code == 200:
-    print(response.text)
+    data = json.loads(response.text)
+    pprint.pprint(data)
 else:
     print(f"Error: {response.status_code}")
